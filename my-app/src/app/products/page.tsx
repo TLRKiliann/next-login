446d23b092ff8c3dc379a3eefea48770623c5958
TLRKiliann <philogenie@protonmail.com>
2024-02-14 16:20:39 +0100

    fake call API done

--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,19 +1,40 @@
-import type { Metadata } from 'next'
-import React from 'react'
+"use client";
 
-export const metadata: Metadata = {
-    title: "Products",
-    description: "access accepted"
-};
+import type { ProductsProps } from '../lib/definitions';
+import React, { useState, useEffect } from 'react'
 
 export default function page() {
+
+  const [products, setProducts] = useState<ProductsProps[]>([]);
+
+  useEffect(() => {
+    const callerProd = async () => {
+      const res = await fetch("/api/products");
+      const data = await res.json();
+      setProducts(data)
+    }
+    callerProd();
+    return () => console.log("clean-up products")
+  }, [])
+
+  if (products.length > 3) {
+    throw new Error("Some trouble with products")
+  }
+
   return (
     <div>    
         <h1 className='text-2xl p-4'>
-            Products
+            Products Page
         </h1>
 
-        <p className='text-xl p-4'>Outlet:</p>
+        {products.map((prod: ProductsProps) => (
+          <div key={prod.id}>
+            <p>Product: {prod.productName}</p>
+            <p>Price: {prod.price}</p>
+            <p>Stock: {prod.stock}</p>
+            <p>Ref: {prod.ref}</p>
+          </div>
+        ))}
 
     </div>
   )
