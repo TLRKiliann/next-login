25d2e31bb8fbb9a6af35ec3b7f1edd33db3cb970
TLRKiliann <philogenie@protonmail.com>
2024-02-14 16:22:10 +0100

    route to call product by params id done

---
+++ my-app/src/app/api/products/[id]/route.ts
@@ -0,0 +1,13 @@
+import type { ProductsProps } from "@/app/lib/definitions";
+import { products } from "@/app/lib/data";
+import { redirect } from "next/navigation";
+
+export async function GET(request: Request, {params}: {params: {id: string}}) {
+    if (parseInt(params.id) > products.length) {
+        redirect('/products')
+    };
+    const findById = products.find(
+        (product: ProductsProps) => product.id === parseInt(params.id)
+    );
+    return Response.json(findById);
+};
