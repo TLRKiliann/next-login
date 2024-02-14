3f57e7624eeb4409cc52c8a48f38d397d17e3542
TLRKiliann <philogenie@protonmail.com>
2024-02-14 15:37:59 +0100

    animation btn effect improved

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,24 +1,21 @@
 "use client";
 
-// importing necessary functions
 import { useSession, signIn, signOut } from "next-auth/react"
 import Link from 'next/link';
 import Image from "next/image";
 
 export default function Home() {
-  // extracting data from usesession as session
+
   const { data: session } = useSession()
 
-  // checking if sessions exists
   if (session) {
-    // rendering components for logged in users
     return (
       <div className="w-full h-screen flex flex-col justify-center items-center">
         <div className="w-44 h-44 relative mb-4">
         <Image
           src={session.user?.image as string}
           fill
-          alt=""
+          alt="img profile session"
           className="object-cover rounded-full"
         />
         </div>
@@ -29,7 +26,8 @@ export default function Home() {
         <p className="font-bold mb-4">
           {session.user?.email}
         </p>
-        <button className="text-slate-50 bg-red-600/90 py-2 px-6 rounded-md" 
+        <button className="text-slate-50 bg-red-600/90 transition trasform duration-200 
+          hover:scale-105 py-2 px-6 rounded-md hover:shadow-lg" 
           onClick={() => signOut()}>
           Sign out
         </button>
@@ -42,7 +40,6 @@ export default function Home() {
     )
   }
 
-  // rendering components for not logged in users
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
         <p className="text-2xl mb-2">
@@ -62,5 +59,4 @@ export default function Home() {
         </button>
     </div>
   )
-
 }
