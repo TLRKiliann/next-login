9ce60a59c081d2ed2d3aceeb9c007298551d016f
TLRKiliann <philogenie@protonmail.com>
2024-02-15 18:19:55 +0100

    link added to reach /about route

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -8,6 +8,11 @@ export default async function Home() {
       >
         Go to products
       </Link>
+      <Link href="/about" className="btn-primary mb-4"
+      >
+        Go to about
+      </Link>
+
       <legend className="italic">(you will be redirect to login)</legend>
 
     </div>
